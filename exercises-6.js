2952bed8eeac36b28e3da99629b4d30d1e2edbe8
anandapuja <anandapujawandra@gmail.com>
2020-02-02 23:38:34 +0700

    meneruskan mengembalikan palindrome angka

--- exercises-6.js
+++ exercises-6.js
@@ -1,31 +1,25 @@
 function angkaPalindrome(num) {
     // var num = 117;
-    var strNum = num.toString();
+    var strNum = String(num);
     var balikNum = ""; //String yang terbalik
-    
+
     for( i = strNum.length -1; i >= 0; i-- ) {
-        balikNum = balikNum + strNum[i];
+        balikNum += strNum[i];
     }
-    // console.log(balikNum + '//////');
-    // var setelahNum = "";
-    
+
     // num++;
     for( i = num; i > 0; i++ ) {
-        if( strNum == balikNum ) {
+        if( num === balikNum ) {
             return balikNum;
         }
         break;
     }
 }
-
-    // console.log(strNum);
-    // console.log(balikNum);
-//   }
   
   // TEST CASES
 //   console.log(angkaPalindrome(8)); // 9
-  console.log(angkaPalindrome(10)); // 11
-//   console.log(angkaPalindrome(117)); // 121
+  // console.log(angkaPalindrome(10)); // 11
+  console.log(angkaPalindrome(117)); // 121
 //   console.log(angkaPalindrome(175)); // 181
 //   console.log(angkaPalindrome(1000)); // 1001
 
