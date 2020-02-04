a714eb066a586ea7de8c610caafc1f9265d2d09e
anandapuja <anandapujawandra@gmail.com>
2020-02-04 13:18:31 +0700

    selesai perkalian unik

--- exercises-10.js
+++ exercises-10.js
@@ -1,17 +1,24 @@
+// Logic Challenge - Perkalian Unik
+
 function perkalianUnik(arr) {
-    var arrKali;
-    for( i = arr.length -1; i > 0; i-- ) {
-        for( j = 0; j <= i; j++ ) {
-            var newArr = arr[j + 1] * ;
-            console.log(newArr);
-            
+    
+    var arrHasil = []; // Tampung hasil akhir
+    for( i = 0; i < arr.length; i++ ){ // Looping start perkalian masing2 indeks
+        var hasilKali = 1;
+        for( j = 0; j < arr.length; j++){ // Loopinging hasil kalinya
+            if( j != i ){
+                hasilKali *= arr[j];
+            }
         }
+        arrHasil.push(hasilKali); // Masukkan tiap nilai akhir hasil kali ke var penampung
     }
+    return arrHasil;
+    
   }
   
   // TEST CASES
   console.log(perkalianUnik([2, 4, 6])); // [24, 12, 8]
-//   console.log(perkalianUnik([1, 2, 3, 4, 5])); // [120, 60, 40, 30, 24]
-//   console.log(perkalianUnik([1, 4, 3, 2, 5])); // [120, 30, 40, 60, 24]
-//   console.log(perkalianUnik([1, 3, 3, 1])); // [9, 3, 3, 9]
-//   console.log(perkalianUnik([2, 1, 8, 10, 2])); // [160, 320, 40, 32, 160]
+  console.log(perkalianUnik([1, 2, 3, 4, 5])); // [120, 60, 40, 30, 24]
+  console.log(perkalianUnik([1, 4, 3, 2, 5])); // [120, 30, 40, 60, 24]
+  console.log(perkalianUnik([1, 3, 3, 1])); // [9, 3, 3, 9]
+  console.log(perkalianUnik([2, 1, 8, 10, 2])); // [160, 320, 40, 32, 160]
