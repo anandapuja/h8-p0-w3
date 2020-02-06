39bea93d3596a42a72b6e7aaf043d3d141196e72
anandapuja <anandapujawandra@gmail.com>
2020-02-06 12:38:54 +0700

    selesai algoritma dan pengelompokan hewan

--- exercises-15.js
+++ exercises-15.js
@@ -1,7 +1,19 @@
 // Logic Challenge - Mengelompokkan Hewan
 
+/**
+ 1. Urutkan hewan sesuai abjad.
+ 2. Buat kondisi pembanding, isi dengan hewan pertama.
+ 3. Bandingkan masing2 hewan dengan pembanding, jika sama masukkan hewan ke var penampung
+ 4. Jika beda, push dulu penampung ke hasil.
+    - Kosongkan lagi penampung
+    - Isi penampung dengan data terkini
+    - Ubah pembanding dengan data terkini.
+ 5. Push data yang masih ada di penampung terakhir ke hasil.
+ */
+
 function groupAnimals(animals) {
 
+    // Sorting Manual
     var sorting = [];
     for( i = 0; i < animals.length; i++ ) {
         for( j = 0; j <animals.length; j++ ) {
